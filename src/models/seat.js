ce2a9ce42945401e702aaaaacb858edb33944589
SaraswatiKumari1 <kri.saras123@gmail.com>
2025-11-16 20:01:10 +0530

    Added api for getting flight by user id

--- src/models/seat.js
+++ src/models/seat.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Airplane, {
+      this.belongsTo(models.Airplanes, {
         foreignKey: 'airplaneId'
       });
     }
